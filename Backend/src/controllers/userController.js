d54fe6a8ee4aae22a7df7d3ff3be7fa27177c4c8
Sanhil143 <sanhilrai143@gmail.com>
2023-05-19 23:30:48 +0530

    create signup and signin api

---
+++ Backend/src/controllers/userController.js
@@ -0,0 +1,27 @@
+const userModel = require('../models/userModel');
+
+
+
+
+const createUser = async (req, res) => {
+      try {
+            let data = req.body;
+            const { name, email, password } = data;
+            if (!name || !email || !password) {
+                  return res.status(400).send({ status: false, message: 'Please enter all fields' })
+            }
+            const userExists = await userModel.findOne({ email })
+            if (userExists) {
+                  return res.status(400).send({ status: false, message: 'user already exist' })
+            }
+            const user = await userModel.create(data)
+            if (user) {
+                  return res.status(201).send({ status: true, message: 'user created succesfully' })
+            }
+      } catch (err) {
+            return res.status(500).send({ status: false, error: err.message })
+      }
+}
+
+
+module.exports = { createUser }
