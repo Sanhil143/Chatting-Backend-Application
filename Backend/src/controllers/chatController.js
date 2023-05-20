888501587671a8297008d7aceff3c946ef19ff22
Sanhil143 <sanhilrai143@gmail.com>
2023-05-21 00:51:21 +0530

    completed creatingChat & fetchingChat api

--- Backend/src/controllers/chatController.js
+++ Backend/src/controllers/chatController.js
@@ -3,45 +3,67 @@ const chatModel = require('../models/chatModel')
 const userModel = require('../models/userModel')
 
 
-const createChat = async (req,res) => {
-      const {userId} = req.body;
+const createChat = async (req, res) => {
+      const { userId } = req.body;
 
-      if(!userId){
-            return res.status(400).send({status:false, message:'Something went wrong with user param'});
+      if (!userId) {
+            return res.status(400).send({ status: false, message: 'Something went wrong with user param' });
       }
 
       var isChat = await chatModel.find({
-            isGroupChat:false,
-            $and:[
-                  {users:{$elemMatch:{$eq:req.user._id}}},
-                  {users:{$elemMatch:{$eq:userId}}},
+            isGroupChat: false,
+            $and: [
+                  { users: { $elemMatch: { $eq: req.user._id } } },
+                  { users: { $elemMatch: { $eq: userId } } },
             ],
       })
-      .populate('users','-password')
-      .populate('latestMessage');
+            .populate('users', '-password')
+            .populate('latestMessage');
 
-      isChat = await userModel.populate(isChat,{
-            path:'latestMessage.sender',
-            select:'name picture email'
+      isChat = await userModel.populate(isChat, {
+            path: 'latestMessage.sender',
+            select: 'name picture email'
       });
-      
-      if(isChat.length > 0){
+
+      if (isChat.length > 0) {
             res.send(isChat[0]);
-      }else{
+      } else {
             var chatData = {
-                  chatName:'sender',
-                  isGroupChat:false,
-                  users:[req.user._id, userId],
+                  chatName: 'sender',
+                  isGroupChat: false,
+                  users: [req.user._id, userId],
             };
             try {
                   const createdChat = await chatModel.create(chatData);
-                  const fullChat = await chatModel.findOne({_id:createdChat._id})
-                  .populate('users','-password');
-                  return res.status(200).send({status:true, fullChat});
+                  const fullChat = await chatModel.findOne({ _id: createdChat._id })
+                        .populate('users', '-password');
+                  return res.status(200).send({ status: true, fullChat });
             } catch (error) {
-                  return res.status(500).send({status:false, message:error.message});
+                  return res.status(500).send({ status: false, message: error.message });
             }
       }
 }
 
-module.exports = {createChat}
+const fetchChat = async (req, res) => {
+      try {
+            let data = await chatModel.find({
+                  users: { $elemMatch: { $eq: req.user._id } }
+            }).populate('users', '-password')
+                  .populate('groupAdmin', '-password')
+                  .populate('latestMessage')
+                  .sort({ updatedAt: -1 })
+                  .then(async (res1) => {
+                        res1 = await userModel.populate(res1, {
+                              path: "latestMessage.sender",
+                              select: "name picture email",
+                        })
+                        return res.status(200).send({ status: true, res1 });
+
+                  })
+                  console.log(data);
+      } catch (error) {
+            return res.status(500).send({ status: false, message: error.message })
+      }
+}
+
+module.exports = { createChat, fetchChat }
--- Backend/src/routes/chatRoute.js
+++ Backend/src/routes/chatRoute.js
@@ -1,10 +1,12 @@
 
 const router = require('express').Router();
-const { createChat } = require('../controllers/chatController');
+const { createChat, fetchChat } = require('../controllers/chatController');
 const {auth} = require('../middlewares/authMiddleware')
 
 
 router.post('/createChat',auth,createChat);
+
+router.get('/fetchChat', auth, fetchChat)
 
 
 
